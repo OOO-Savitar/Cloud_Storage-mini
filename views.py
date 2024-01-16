9ffe139f949709f7c0e7cbb3e652bd4d34314432
_-=Miyagi=-_ <ppamuzhakk@mail.ru>
2024-01-16 17:56:17 +0300

    Немного исправлена логика. Добавлена смена тем

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
 from flask import render_template, request, jsonify
 from funcs import *
+import json
+
+with open('config.json', 'r', encoding='utf-8') as file:
+    config_data = json.load(file)
+
+themes_id = {item['name']: item['id'] for item in config_data['backgrounds']}
 
 
 @app.route('/')
 def main():
-    return render_template('index.html', json_data=jsonify({'0': 1}).json)
+    return render_template('index.html', backgrounds=config_data['backgrounds'])
 
 
 @app.route('/check/file/exists')
@@ -63,6 +69,38 @@ def get_all_files():
     images = [{'file': file, 'img': formatting_correct_img(file)} for file in files]
 
     return images
+
+
+@app.route('/set_background', methods=['GET'])
+def return_background():
+    background_name = request.args.get('name', None)
+
+    base_json_request = {
+        'name': background_name,
+    }
+
+    if not background_name:
+        base_json_request['success'] = False
+        return jsonify(base_json_request)
+
+    for item in config_data['backgrounds']:
+        if item.get('name') == background_name:
+            if item.get('additional', {}).get('is_color'):
+                base_json_request['is_color'] = True
+                base_json_request['color'] = item.get('preview')
+                return jsonify(base_json_request), 200
+            else:
+                base_json_request['preview'] = item.get('preview')
+                base_json_request['background'] = item.get('background')
+                return jsonify(base_json_request), 200
+
+
+@app.route('/get-background-section', methods=['GET'])
+def get_background_section():
+    theme = request.args.get('theme', 'base')
+    play_video = request.args.get('play', False) == 'true'
+    item = config_data['backgrounds'][themes_id[theme]]
+    return jsonify(render_template('background__main_template.html', play=play_video, theme=item))
 
 
 app.run(host='0.0.0.0', debug=True)
